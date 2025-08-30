db4708267666d12e7f8cb87b3fbf62bf10ad141a
Faris Gogić <faris.gogic@edu.fit.ba>
2025-08-30 17:07:34 +0200

    Build Vite app into docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/To-Do/',
   plugins: [
     vue(),
   ],
@@ -13,5 +12,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    outDir: 'docs'
+  },
 })
